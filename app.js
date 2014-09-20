191707f49ccd53468c71a0c005135646914705ad
Ivan Kamyshan <ivankamyshan@gmail.com>
2014-09-20 21:25:39 +0300

    return layout

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.configure(function() {
     app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
-    app.set('view engine', 'html')                      
+    app.set('view engine', 'html')
     app.set('views', __dirname + "/tpl");
-    app.set("view options", 'index.html');
+    app.set("view options", {layout: 'layout.html'});
     app.use(express.static(__dirname + "/public"));     // Делаем файлы из папки public доступными на сайте
 });
 
@@ -13,6 +13,6 @@ app.get('/', function(req, res){          // Обрабатываем запро
     res.render('index.html');
 });
 
-var port = process.env.PORT || 5000;       
-app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
+var port = process.env.PORT || 5000;
+app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port"
 console.log("Listening at " + port)        // Пишем в консоль, что запустились
