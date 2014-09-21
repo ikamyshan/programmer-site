54e0bb23723805282814099af9bb0fa4bf646523
Ivan Kamyshan <ivankamyshan@gmail.com>
2014-09-21 11:55:39 +0300

    layout false

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.configure(function() {
     app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
     app.set('view engine', 'html')
     app.set('views', __dirname + "/tpl");
-    app.set("view options", {layout: 'layout.html'});
+    app.set("view options", {layout: false});
     app.use(express.static(__dirname + "/public"));     // Делаем файлы из папки public доступными на сайте
 });
 
